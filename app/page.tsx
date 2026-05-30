6435801178a876018d70be04049e676ad2e7f55d
Hikari1011 <g2460222@edu.cc.ocha.ac.jp>
2026-05-30 17:01:00 +0900

    ホーム画面の背景を入れました

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,19 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <div className="text-center bg-white">hello world</div>
-      <Link href='/exchange'>click me</Link><br></br>
-      <Link href='/book'>book</Link>
+    <div
+      className="min-h-screen bg-cover bg-center"
+      
+    >
+      <div className="text-center text-white text-3xl pt-20">
+        hello world
+      </div>
+
+      <div className="text-center mt-10">
+        <Link href="/exchange">click me</Link>
+        <br />
+        <Link href="/book">book</Link>
+      </div>
     </div>
   );
 }
