3b2fd657744ebbea2f87a1c334908775e62362da
lktoby <tobyleung112@gmail.com>
2026-05-30 18:40:19 +0900

    bottom nav bar update

--- components/bottom-nav.tsx
+++ components/bottom-nav.tsx
@@ -1,22 +1,33 @@
 import Link from "next/link";
 import Image from "next/image";
+import styles from '../components/styles/botton-nav.module.css';
 
 export function BottomNav() {
     return (
         <div>
-            <nav className="flex justify-around items-end text-center">
-                <ul className="flex flex-col items-center">
-                    <li><Link href='/' passHref><Image src='/bear.png' alt='home page' height={100} width={100}></Image></Link></li>
-                    <li><Link href='/' className="">ホーム</Link></li>
-                </ul>
-                <ul className="flex flex-col items-center">
-                    <li><Link href='/exchange'><Image src='/dog1.png' alt='exchange' height={100} width={100}></Image></Link></li>
-                    <li><Link href='/exchange'>こうかん</Link></li>
-                </ul>
-                <ul className="flex flex-col items-center">
-                    <li><Link href='/book'><Image src='/cat1.png' alt='collection' height={100} width={100}></Image></Link></li>
-                    <li><Link href='/book'>シールちょう</Link></li>
-                </ul>
+            <nav className={styles.nav}>
+                <div className="flex justify-around items-end text-center flex-stretch">
+                    <button className="grow focus:opacity-60 focus:bg-purple-200 focus:rounded-xl hover:opacity-60">
+                        <ul className="flex flex-col items-center">
+                            <li><Link href='/'><Image src='/bear.png' alt='home page' height={100} width={100}></Image></Link></li>
+                            <li><Link href='/' className="">ホーム</Link></li>
+                        </ul>
+                    </button>
+                    <button className="grow focus:opacity-60 focus:bg-purple-200 focus:rounded-xl hover:opacity-60">
+                        <ul className="flex flex-col grow items-center">
+                        <li><Link href='/exchange'><Image src='/dog1.png' alt='exchange' height={100} width={100}></Image></Link></li>
+                        <li><Link href='/exchange'>こうかん</Link></li>
+                    </ul>
+                    </button>
+                    <button className="grow focus:opacity-60 focus:bg-purple-200 focus:rounded-xl hover:opacity-60">
+                        <ul className="flex flex-col grow items-center">
+                            <li><Link href='/book'><Image src='/cat1.png' alt='collection' height={100} width={100}></Image></Link></li>
+                            <li><Link href='/book'>シールちょう</Link></li>
+                        </ul>
+                    </button>
+                    
+                </div>
+                
             </nav>
         </div>
     )
