336b1e2f50f0650ee40178c28b1bda0a8702ee08
Hikari1011 <g2460222@edu.cc.ocha.ac.jp>
2026-05-30 23:37:32 +0900

    シール帳のページを更新しました

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,17 +1,35 @@
-import { BottomNav } from "@/components/bottom-nav"
-import Link from "next/link"
+import { BottomNav } from "@/components/bottom-nav";
+import { Badge } from "@/components/ui/badge";
 
 export default function Book() {
-    return (
-        <div
-      className="min-h-screen bg-[url('/background.png')] bg-cover bg-center relative">
-        
-        <div>
-            <div>あつめたシールたち</div>
-            <div className="absolute inset-x-0 bottom-0">
-                <BottomNav></BottomNav>
-            </div>
+  return (
+    <div className="min-h-screen bg-[url('/background.png')] bg-cover bg-center relative">
+
+     
+      <div className="flex justify-center">
+        <div className="p-6 text-3xl font-bold">
+        <Badge>あつめたシールたち</Badge>
         </div>
+      </div>
+      
+
+      
+      <div className="flex justify-center">
+        <div className="grid grid-cols-8 gap-8 p-5 pb-24">
+          {Array.from({ length: 24 }).map((_, i) => (
+            <div
+              key={i}
+              className="w-16 h-16 bg-white rounded-xl shadow border border-gray-100"
+            />
+          ))}
         </div>
-    )
+      </div>
+
+      {/* BottomNav */}
+      <div className="absolute inset-x-0 bottom-0">
+        <BottomNav />
+      </div>
+
+    </div>
+  );
 }
