6f356d1ef051c9ebc0821124a11fbd0829d40be3
Hikari1011 <g2460222@edu.cc.ocha.ac.jp>
2026-05-30 18:10:53 +0900

    ホーム画面のボタン作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,20 @@
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default function ButtonUsage() {
   return (
     <div
-      className="min-h-screen bg-cover bg-center"
-      
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
     >
-      <div className="text-center text-white text-3xl pt-20">
-        hello world
-      </div>
-
-      <div className="text-center mt-10">
-        <Link href="/exchange">click me</Link>
-        <br />
-        <Link href="/book">book</Link>
+      <div style={{ transform: "translateY(40px)" }}>
+        <Button style={{ fontFamily: "NicoMoji" }}>
+        きょうのシールをゲットしよう
+        </Button>
       </div>
     </div>
   );
